b9a46480c41c61265fea4127d5843ce90e3756df
Miqueias Teofani <miqueiasteofaniB@outlook.com>
2018-11-27 12:52:05 -0300

    Conecção com a APITecnicos

--- RATEletronica/RATEletronica/Atendimentos.xaml.cs
+++ RATEletronica/RATEletronica/Atendimentos.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using CarouselView.FormsPlugin.Abstractions;
 using CarouselView.FormsPlugin.Android;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,95 +17,153 @@ namespace RATEletronica
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Atendimentos : CarouselPage/*ContentPage*/
 	{
-
-		public Atendimentos ()
+        public static HttpClient cliente;
+        public static string NomeTecnico = "Tecnico1";
+        
+        public Atendimentos ()
 		{
             
 
             InitializeComponent();
             Title = "Atendimentos Pendentes";
-            foreach (var item in CarroselAtendimentos())
-            {
-                Children.Add(item);
-            }
+
+            CarroselAtendimentosAsync();
 
         }
-        public List<Controles.Atendimento> ListaAtendimentos()
+        public async Task<List<Controles.Atendimento>> ListaAtendimentosAsync()
         {
-            var ListaAtendimentos = new List<Controles.Atendimento>(){
-               new Controles.Atendimento(1,1,1,"Serie1",Controles.Atendimento.TipoAtendimentos.Instalação,DateTime.Now,DateTime.Now,1,new TimeSpan(1,1,1),new TimeSpan(1,1,1),new TimeSpan(1,1,1),Controles.Atendimento.Itinerarios.BC,1,Controles.Atendimento.Itinerarios.CB,1,Controles.Atendimento.Condicaofinal.Parado,Controles.Atendimento.CondicaoMaterial.Necessita_peças,"Obs1"),
-               new Controles.Atendimento(2,1,1,"Serie2",Controles.Atendimento.TipoAtendimentos.Instalação,DateTime.Now,DateTime.Now,1,new TimeSpan(1,1,1),new TimeSpan(1,1,1),new TimeSpan(1,1,1),Controles.Atendimento.Itinerarios.BC,1,Controles.Atendimento.Itinerarios.CB,1,Controles.Atendimento.Condicaofinal.Parado,Controles.Atendimento.CondicaoMaterial.Necessita_peças,"Obs1"),
-               new Controles.Atendimento(3,1,1,"Serie3",Controles.Atendimento.TipoAtendimentos.Instalação,DateTime.Now,DateTime.Now,1,new TimeSpan(1,1,1),new TimeSpan(1,1,1),new TimeSpan(1,1,1),Controles.Atendimento.Itinerarios.BC,1,Controles.Atendimento.Itinerarios.CB,1,Controles.Atendimento.Condicaofinal.Parado,Controles.Atendimento.CondicaoMaterial.Necessita_peças,"Obs1"),
-            };
-
-            var listStr = new List<string>();
-
-            foreach (var item in ListaAtendimentos)
+            try
             {
-                listStr.Add(item.Serie);
+                HttpClient client = new HttpClient();
+                string url = "http://tecnicos.gearhostpreview.com/api/Atendimentos?tecnico=Tecnico1";
+                var response = await client.GetStringAsync(url);
+                var autenticidade = JsonConvert.DeserializeObject<bool>(response);
+                NomeTecnico = "Tecnico1";
+                if (autenticidade)
+                {
+                    var uri2 = "http://tecnicos.gearhostpreview.com/api/Atendimentos";
+                    var response2 = await client.GetStringAsync(uri2);
+                    var listaAtendimentos = JsonConvert.DeserializeObject<List<Controles.Atendimento>>(response2);
+
+                    return listaAtendimentos;
+                }
+
+                return null;
+
             }
-
-
-            return ListaAtendimentos;
+            catch (Exception ex)
+            {
+                return null;
+                
+            }
         }
-        public List<ContentPage> CarroselAtendimentos()
+        #region Teste
+        //    var ListaAtendimentos = new List<Controles.Atendimento>(){
+        //   new Controles.Atendimento(1,1,1,"Serie1",Controles.Atendimento.TipoAtendimentos.Instalação,DateTime.Now,DateTime.Now,1,new TimeSpan(1,1,1),new TimeSpan(1,1,1),new TimeSpan(1,1,1),Controles.Atendimento.Itinerarios.BC,1,Controles.Atendimento.Itinerarios.CB,1,Controles.Atendimento.Condicaofinal.Parado,Controles.Atendimento.CondicaoMaterial.Necessita_peças,"Obs1"),
+        //   new Controles.Atendimento(2,1,1,"Serie2",Controles.Atendimento.TipoAtendimentos.Instalação,DateTime.Now,DateTime.Now,1,new TimeSpan(1,1,1),new TimeSpan(1,1,1),new TimeSpan(1,1,1),Controles.Atendimento.Itinerarios.BC,1,Controles.Atendimento.Itinerarios.CB,1,Controles.Atendimento.Condicaofinal.Parado,Controles.Atendimento.CondicaoMaterial.Necessita_peças,"Obs1"),
+        //   new Controles.Atendimento(3,1,1,"Serie3",Controles.Atendimento.TipoAtendimentos.Instalação,DateTime.Now,DateTime.Now,1,new TimeSpan(1,1,1),new TimeSpan(1,1,1),new TimeSpan(1,1,1),Controles.Atendimento.Itinerarios.BC,1,Controles.Atendimento.Itinerarios.CB,1,Controles.Atendimento.Condicaofinal.Parado,Controles.Atendimento.CondicaoMaterial.Necessita_peças,"Obs1"),
+        //};
+
+        //var listStr = new List<string>();
+
+        //foreach (var item in ListaAtendimentos)
+        //{
+        //    listStr.Add(item.Serie);
+        //}
+
+
+        //return ListaAtendimentos;
+        #endregion
+    
+        public async void CarroselAtendimentosAsync()
         {
-            List<ContentPage> pag = new List<ContentPage>(0);
-            ListaAtendimentos();
+        List<ContentPage> pag = new List<ContentPage>(0);
+        List<Controles.Atendimento> lista = await ListaAtendimentosAsync();
 
-            foreach (var item in ListaAtendimentos())
+        foreach (var item in lista)
+        {
+            if (item.IdAtendimento == lista.Last().IdAtendimento)
             {
-                if (item.IdAtendimento == ListaAtendimentos().Last().IdAtendimento)
+                    
+                    pag.Add(new ContentPage
+                    {
+                        Content = new StackLayout
+                        {
+                            Children = {
+                            { new Label
+                                {
+                                    Text =  "Tecnico:  "+NomeTecnico , FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label))
+                                } },
+
+                            { new Label
+                                {
+                                    Text =  "Data Prevista:  "+item.DataPrevista.Value.ToString("dd/MM/yyyy") , FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label))
+                                } },
+                            { new Label
+                                {
+                                    Text = "Serie:  "+item.Serie.ToString(), FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label))
+                                } },
+                            { new Label
+                                {
+                                    Text = "Requisição:  "+item.IdRequisicao.ToString(), FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label))
+                                } },
+                            { new Label
+                                {
+                                    Text = " < ", FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)),TextColor=Color.Blue, HorizontalTextAlignment= TextAlignment.Center
+                                } },
+                            { new Button
+                                {
+                                    Text = "Editar/Encerrar", BackgroundColor = Color.Blue , BorderColor = Color.Black , TextColor = Color.White
+                                } }
+
+                        }
+                    }
+                });
+            }else
+            if (item.IdAtendimento == lista.First().IdAtendimento)
+            {
+                pag.Add(new ContentPage
                 {
-                    pag.Add(new ContentPage
+                    Content = new StackLayout
                     {
-                        Content = new StackLayout
-                        {
-                            Children = {
-                                { new Label { Text =  "Data Prevista: "+item.DataPrevista.ToString() , FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)) } },
-                                { new Label { Text = "Serie: "+item.Serie.ToString(), FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label))  } },
-                                { new Label { Text = "Requisição: "+item.IdRequisicao.ToString(), FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)) } },
-                                { new Label { Text = " < ", FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)),TextColor=Color.Blue, HorizontalTextAlignment= TextAlignment.Center } },
-                                { new Button { Text = "Editar/Encerrar", BackgroundColor = Color.Blue , BorderColor = Color.Black , TextColor = Color.White } }
+                        Children = {
+                            { new Label
+                                {
+                                    Text =  "Tecnico:  "+NomeTecnico , FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label))
+                                } },
+                            { new Label { Text =  "Data Prevista: "+item.DataPrevista.Value.ToString("dd/MM/yyyy") , FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)) } },
+                            { new Label { Text = "Serie: "+item.Serie.ToString(), FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label))  } },
+                            { new Label { Text = "Requisição: "+item.IdRequisicao.ToString(), FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)) } },
+                            { new Label { Text = " > ", FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)),TextColor=Color.Blue, HorizontalTextAlignment= TextAlignment.Center } },
+                            { new Button { Text = "Editar/Encerrar", BackgroundColor = Color.Blue , BorderColor = Color.Black , TextColor = Color.White } }
 
-                            }
                         }
-                    });
-                }else
-                if (item.IdAtendimento == ListaAtendimentos().First().IdAtendimento)
+                    }
+                });
+            }else               
+                pag.Add(new ContentPage
                 {
-                    pag.Add(new ContentPage
+                    Content = new StackLayout
                     {
-                        Content = new StackLayout
-                        {
-                            Children = {
-                                { new Label { Text =  "Data Prevista: "+item.DataPrevista.ToString() , FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)) } },
-                                { new Label { Text = "Serie: "+item.Serie.ToString(), FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label))  } },
-                                { new Label { Text = "Requisição: "+item.IdRequisicao.ToString(), FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)) } },
-                                { new Label { Text = " > ", FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)),TextColor=Color.Blue, HorizontalTextAlignment= TextAlignment.Center } },
-                                { new Button { Text = "Editar/Encerrar", BackgroundColor = Color.Blue , BorderColor = Color.Black , TextColor = Color.White } }
+                        Children = {
+                            { new Label
+                                {
+                                    Text =  "Tecnico:  "+NomeTecnico , FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label))
+                                } },
+                            { new Label { Text =  "Data Prevista: "+item.DataPrevista.Value.ToString("dd/MM/yyyy") , FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)) } },
+                            { new Label { Text = "Serie: "+item.Serie.ToString(), FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label))  } },
+                            { new Label { Text = "Requisição: "+item.IdRequisicao.ToString(), FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)) } },
+                            { new Label { Text = "< -- > ", FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)),TextColor=Color.Blue, HorizontalTextAlignment= TextAlignment.Center } },
+                            { new Button { Text = "Editar/Encerrar", BackgroundColor = Color.Blue , BorderColor = Color.Black , TextColor = Color.White } }
 
-                            }
                         }
-                    });
-                }else               
-                    pag.Add(new ContentPage
-                    {
-                        Content = new StackLayout
-                        {
-                            Children = {
-                                { new Label { Text =  "Data Prevista: "+item.DataPrevista.ToString() , FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)) } },
-                                { new Label { Text = "Serie: "+item.Serie.ToString(), FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label))  } },
-                                { new Label { Text = "Requisição: "+item.IdRequisicao.ToString(), FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)) } },
-                                { new Label { Text = "< -- > ", FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)),TextColor=Color.Blue, HorizontalTextAlignment= TextAlignment.Center } },
-                                { new Button { Text = "Editar/Encerrar", BackgroundColor = Color.Blue , BorderColor = Color.Black , TextColor = Color.White } }
-
-                            }
-                        }
-                    });         
-            }
-            return pag;
-
+                    }
+                });         
+        }
+        foreach (var item in pag)
+        {
+            Children.Add(item);
+        }
 
         }
         
--- RATEletronica/RATEletronica/MainPage.xaml.cs
+++ RATEletronica/RATEletronica/MainPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -13,5 +15,41 @@ namespace RATEletronica
 		{
 			InitializeComponent();
 		}
-	}
+
+        private async void btnLogar_ClickedAsync(object sender, EventArgs e)
+        {
+            if (lbUsuario.Text != "")
+            {
+                if (lbSenha.Text != "")
+                {
+                    await AutenticarAsync(lbUsuario.Text);
+
+                }
+                else { }
+
+            }
+            else { }
+        }
+        private async Task<bool> AutenticarAsync(string tecnico)
+        {
+            try
+            {
+                HttpClient client =new HttpClient();
+                string url = "http://tecnicos.gearhostpreview.com/api/Atendimentos?tecnico="+tecnico;
+                var response = await client.GetStringAsync(url);
+                var autenticidade = JsonConvert.DeserializeObject<bool>(response);
+
+                if (autenticidade)
+                {
+                    await Navigation.PushModalAsync(new Atendimentos());
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
 }
